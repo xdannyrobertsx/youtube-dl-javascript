5bbcd9e1857921eaa4435a6ed39edd39053aec1c
danny <64439795+xdannyrobertsx@users.noreply.github.com>
2022-02-09 13:12:19 -0600

    Clean Up and Adding API Key

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let channelID = "exampleID123";
 
-let apiKey = "exampleKey234";
+let apiKey = "AIzaSyAWIrzsVnN0QO_gukNz5I37BNya2GuZe9s";
+
+const https = require("https");
+
+let fetchYoutube = function (json) {
+  https
+    .get(`${json}`, (res) => {
+      let data = "";
+
+      // called when a data chunk is received.
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // called when the complete response is received.
+      res.on("end", () => {
+        console.log(JSON.parse(data));
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: ", err.message);
+    });
+};
 
 function getChannelID(channel) {
   let rx = /channel\/(.*)/;
@@ -10,16 +32,9 @@ function getChannelID(channel) {
   console.log(arr[1]);
 }
 
-let bruh = "https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw";
+let userInput = "https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw";
 
-getChannelID(bruh);
+getChannelID(userInput);
 
-let apiCall = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelID}&part=snippet,id&order=date&maxResults=20`;
-
-async function getChannel() {
-  let response = await fetch(apiCall);
-  let data = response.json();
-  return data;
-}
-
-getChannel().then((data) => console.log(data));
+let apiCall = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelID}&part=snippet,id&order=date&maxResults=10`;
+fetchYoutube(apiCall);
---
+++ concept.md
@@ -0,0 +1,4 @@
+#Youtube-DL App
+
+Takes in user input and makes an api request to youtube which returns the last 10 videos for the specified channel.
+The app will then download those 10 videos as wav files.
