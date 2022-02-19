d83217a9642043539bf4698e45ee965e05a24129
danny <64439795+xdannyrobertsx@users.noreply.github.com>
2022-02-19 16:50:04 -0600

    Rewrote App Using Inquirer
    Replaced Prompt with Inquirer to Handle User Input.
    
    Added Code to Create Directory Titled After Channel Name.

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-import prompt from "prompt";
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
 import youtubedl from "youtube-dl-exec";
 
 function prompter() {
-  let channelID = "";
+  let channelID;
+  let vidNumber;
+  const questions = [
+    {
+      type: "input",
+      name: "channel",
+      message: "What Youtube Channel Would You Like To Scrape?",
+      validate(value) {
+        const pass = value.match(/(\/channel\/|c\/(.*))/);
+        if (pass) {
+          return true;
+        }
 
-  let promptReq = {
-    properties: {
-      channel: {
-        pattern: /(\/channel\/|c\/(.*))/,
-        message: "That's not a Youtube Channel!",
-        required: true,
+        return "Please Enter a Valid Youtube Channel";
       },
     },
-  };
+    {
+      type: "input",
+      name: "vidNumber",
+      message: "How Many Videos Would You Like to Scrape?",
+      validate(value) {
+        const pass = value.match(/^(0?[1-9]|[1-9][0-9])$/);
+        if (pass) {
+          return true;
+        }
 
-  prompt.start();
-  console.log("What Youtube Channel Would You Like To Scrape?");
+        return "Please Enter a Number Between 1 and 99";
+      },
+      default() {
+        return 1;
+      },
+    },
+  ];
 
-  prompt.get(promptReq, function (err, answer) {
-    channelID = answer.channel; // returns object with key titled "channel"
-    console.log("GETTING VIDEOS");
+  inquirer.prompt(questions).then((answers) => {
+    channelID = answers["channel"];
+    vidNumber = answers["vidNumber"];
+
+    console.log("Downloading Videos now!");
+
     youtubedl(channelID, {
-      playlistEnd: 2,
+      playlistEnd: vidNumber,
       extractAudio: true,
       audioFormat: "mp3",
-      output: "%(title)s.mp3",
+      output: "./%(channel)s/%(title)s.mp3",
       noPart: true,
     });
   });
